e64c1925b9f8de91042cc764f91fce749ea95ea0
Nithin prasad <nithinprasad549@yahoo.com>
2021-10-22 13:23:11 +0300

    Initial commit

---
+++ spring-security-pwd/src/main/java/com/prasad/nithin/security/SpringSecurityPwdApplication.java
@@ -0,0 +1,13 @@
+package com.prasad.nithin.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPwdApplication.class, args);
+	}
+
+}
